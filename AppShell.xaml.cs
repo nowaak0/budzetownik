629236233d633c0070915746dddc7fa3b62bc638
pnowa <pnowaak14@gmail.com>
2025-06-19 23:50:11 +0200

    Dodanie plików projektu

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -9,6 +9,40 @@
             Routing.RegisterRoute("AddCategoryPage", typeof(Views.AddCategoryPage));
             Routing.RegisterRoute("AddExpensePage", typeof(Views.AddExpensePage));
             Routing.RegisterRoute("ViewExpensesPage", typeof(Views.ViewExpensesPage));
+
+            // Załaduj motyw przy starcie aplikacji
+            LoadTheme();
+        }
+
+        private void ThemeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+            SetTheme(e.Value);
+        }
+
+        void SetTheme(bool isDark)
+        {
+            if (Application.Current != null)
+            {
+                // Ustaw motyw aplikacji
+                Application.Current.UserAppTheme = isDark ? AppTheme.Dark : AppTheme.Light;
+
+                // Zapisz preferencję użytkownika
+                Preferences.Set("IsDarkTheme", isDark);
+            }
+        }
+
+        void LoadTheme()
+        {
+            // Odczytaj zapisaną preferencję (domyślnie motyw jasny)
+            var isDark = Preferences.Get("IsDarkTheme", false);
+
+            // Ustaw stan przełącznika bez wywoływania zdarzenia Toggled
+            ThemeSwitch.Toggled -= ThemeSwitch_Toggled;
+            ThemeSwitch.IsToggled = isDark;
+            ThemeSwitch.Toggled += ThemeSwitch_Toggled;
+
+            // Zastosuj wczytany motyw
+            SetTheme(isDark);
         }
     }
 }
